a4465383eaebfbb5c1bbbe7529d45fa4db2add7a
ravitejapotluri <spoorthy.devineni@gmail.com>
2015-11-27 20:57:37 -0500

    fake

--- statsactual/services.js
+++ statsactual/services.js
@@ -1,7 +1,8 @@
 /*@potluri multiple properties are shared across. some times ajax calls have been cached too with following code
  * its used in reportcontroller and logincontroller. search for sharedproperties in thos controllers.
  */
-var sitedevlink = 'http://thunderify.com/statsactual/';//'http://localhost/xampp/spu/stats-2/';//'wrong';//
+var sitedevlink = 'http://localhost/xampp/spu/potluriux.github.io/statsactual/';
+//'http://thunderify.com/statsactual/';//'http://localhost/xampp/spu/stats-2/';//'wrong';//
 
 var str_window = window.location + " ";
 //alert(str_window.split("#")[0]);
